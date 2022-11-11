ca41e5f48941e6b774f1564e68c8e8004b0e7246
Spartan <nimish.lingesh@gmail.com>
2022-11-11 12:15:16 -0800

    init commit

---
+++ connection.php
@@ -0,0 +1,22 @@
+<?php
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'iwzgygmy_root';
+$DATABASE_PASS = 'Mysql_php_123';
+$DATABASE_NAME = 'iwzgygmy_nirvana';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+?>
+
+<!-- $DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = 'mysql';
+$DATABASE_NAME = 'nirvana'; -->
+
+<!-- $DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'shwethas_root';
+$DATABASE_PASS = 'root123';
+$DATABASE_NAME = 'shwethas_babytoys'; -->
